008bf46b7d0fc204b567f8f303eb252bd05580c6
shiva <shiva@shiva.com>
2024-11-22 15:01:28 +0530

    reverted back to dead simple structure

--- src/js/global.js
+++
@@ -1,9 +0,0 @@
-import { loadPageContent } from "./utils/loadPageContent";
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Determine the current slug
-  const currentSlug = new URL(window.location.href).pathname.split("/").pop();
-
-  // Load page content based on the slug
-  loadPageContent(currentSlug);
-});
--- src/js/utils/helpers/loadScript.js
+++
@@ -1,7 +0,0 @@
-export function loadScript(scriptPath) {
-  const script = document.createElement("script");
-  script.src = scriptPath;
-  script.type = "module"; // Ensure it uses ES module syntax if needed
-  script.async = true; // Optional, but recommended to not block rendering
-  document.body.appendChild(script);
-}
