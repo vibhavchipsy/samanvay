d470b9bb0fb90c81a11506c0265531b33cd0937c
shiva <shiva@shiva.com>
2024-11-22 14:40:21 +0530

    folder restructure

--- src/js/utils/loadPageContent.js
+++ src/js/utils/loadPageContent.js
@@ -2,19 +2,19 @@ import { loadHTML } from "./helpers/loadHTML";
 import { loadScript } from "./helpers/loadScript";
 
 // Define the paths for each page's content
-const HEADER_PATH = "/src/components/header/header.html";
-const FOOTER_PATH = "/src/components/footer/footer.html";
+const HEADER_PATH = "/public/components/header/header.html";
+const FOOTER_PATH = "/public/components/footer/footer.html";
 
 //HOME PAGE
-const HOME_PATH = "/src/pages/home/home.html";
-const BANNER_PATH = "/src/pages/home/banner-carousel/banner-carousel.html";
+const HOME_PATH = "/public/pages/home/home.html";
+const BANNER_PATH = "/public/pages/home/banner-carousel/banner-carousel.html";
 
 // ABOUT PAGE
-const ABOUT_PATH = "/src/pages/about/about.html";
+const ABOUT_PATH = "/public/pages/about/about.html";
 
 export function loadPageContent(slug) {
   loadHTML(HEADER_PATH, "header");
-  loadScript("/src/assets/js/header.js");
+  loadScript("/src/js/header/header.js");
 
   const promises = []; // Array to hold promises
 
@@ -22,12 +22,12 @@ export function loadPageContent(slug) {
   if (slug === "") {
     promises.push(loadHTML(HOME_PATH, "app"));
     promises.push(loadHTML(BANNER_PATH, "banner"));
-    promises.push(loadScript("/src/assets/js/home.js", { async: true })); // Load js file
+    promises.push(loadScript("/src/js/home/home.js", { async: true })); // Load js file
   }
 
   if (slug === "about") {
     promises.push(loadHTML(ABOUT_PATH, "app"));
-    promises.push(loadScript("/src/assets/js/about.js", { async: true })); // Load js file
+    promises.push(loadScript("/src/js/about/about.js", { async: true })); // Load js file
   }
 
   promises.push(loadHTML(FOOTER_PATH, "footer"));
